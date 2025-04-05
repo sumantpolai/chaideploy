36e188a278983e307e3f68099dfbf788cc143fd5
sumantpolai <sumantpolai@gmail.com>
2025-04-05 19:50:58 +0530

    index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
+require('dotenv').config() 
 const express = require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
 // import express from "express";
 const app = express()
-const port = 3000
+const port = 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!'
 })
 
 app.get('/twitter',(req,res)=>{
@@ -14,6 +16,10 @@ app.get('/twitter',(req,res)=>{
 
 app.get('/login',(req, res)=>{
  res.send('<h1>please login at chai aur code</h1>')
+})
+
+app.get('/youtube', (req, res)=>{
+  res.send('<h1>Chai aur code</h>')
 })
 
 app.listen(process.env.PORT, () => {
